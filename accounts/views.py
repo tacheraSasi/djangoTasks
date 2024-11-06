7d7fbf1cb071f1a4f8a7412f14e69ca5080f745e
tacheraSasi <tacherasasi@gmail.com>
2024-11-07 02:01:45 +0300

    fixed the register post view function bugs

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -11,7 +14,6 @@ def loginPost(request):
     return render(request,"accounts/login.html")
 
 def register(request):
-    
     return render(request,"accounts/register.html")
 
 def registerPost(request):
@@ -20,6 +22,33 @@ def registerPost(request):
         email = request.POST['email']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
+        
+        if username and email and password and confirm_password:
+            #register logic here
+            if password == confirm_password :
+                try:
+                    user = User.objects.create_user(
+                        username=username,
+                        email=email,
+                        password=password
+                    ) 
+                    user.save()
+                    messages.success(request,"You have successfully registered to django tasks")
+                    return redirect("login")
+                except:
+                    #NOTE:This error will possibly occur if the user already exists
+                    #TODO: I will add a separate logic for checking if the user already exist 
+                    messages.error(request,"Something went wrong failed to register")
+                    redirect('register')
+            else:
+                messages.error(request,"Passwords do not match")
+                return redirect('register')
+                    
+        else:
+            messages.error(request,"Missing field. Please make sure you fill all the inputs")
+            return  redirect('register')   
+    else:
+        return HttpResponse("Invalid method")
 
 @login_required
 def dashboard(request):
